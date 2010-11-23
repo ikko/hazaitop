97a7a67ab6ca6b687fa2abc15bdbfe4dd99784fe
Nandor Komzak <nandor.komzak@gmail.com>
2010-11-23 14:14:40 +0100

    graph teszteléshez

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.site_search  'search', :controller => 'front', :action => 'search'
   map.root :controller => 'front', :action => 'index'
 
   Hobo.add_routes(map)
