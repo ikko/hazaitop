04087a37a15d89ea495b5c745532521af5e9b4a6
ikko karima <miklos.beky@gmail.com>
2010-11-26 19:25:13 +0100

    datumok-info_source

--- app/models/litigation.rb
+++ app/models/litigation.rb
@@ -16,15 +16,15 @@ class Litigation < ActiveRecord::Base
   # --- Permissions --- #
 
   def create_permitted?
-    acting_user.administrator?
+    acting_user.administrator? || acting_user.editor?
   end
 
   def update_permitted?
-    acting_user.administrator?
+    acting_user.administrator? || acting_user.editor?
   end
 
   def destroy_permitted?
-    acting_user.administrator?
+    acting_user.administrator? || acting_user.editor?
   end
 
   def view_permitted?(field)
