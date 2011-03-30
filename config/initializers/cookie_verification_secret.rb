1d69de9d40234fa4d1ee2f474110d2b1c10208a3
Béky Miklós <miklos.beky@gmail.com>
2011-03-30 23:50:13 +0200

    englishules

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -5,4 +5,4 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 # TODO: 2.3.8nál visszatenni a secretet
-ActionController::Base.cookie_verifier_secret = '867d9636f33abd9e0e6b1c05bc331e61a28b39129e11c367982a40a09adc2a14548d7725200393b79563b081d2a133a76e627af21f838ac18678d307247f4f51';
+ActionController::Base.cookie_verifier_secret = '867d9636f33abd9e1110e6b1c05bc331e61a28b39129e11c367982a40a09adc2a14548d7725200393b79563b081d2a133a76e627af21f838ac18678d307247f4f51';
