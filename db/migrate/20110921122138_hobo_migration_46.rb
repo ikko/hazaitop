cce92fb81c44ae57664b4a42cc006042c8dcb2a9
Béky Miklós <miklos.beky@gmail.com>
2011-09-21 14:35:13 +0200

    cikk_felv_migr

---
+++ db/migrate/20110921122138_hobo_migration_46.rb
@@ -0,0 +1,15 @@
+class HoboMigration46 < ActiveRecord::Migration
+  def self.up
+    add_column :articles, :state, :string, :default => "normal"
+    add_column :articles, :key_timestamp, :datetime
+
+    add_index :articles, [:state]
+  end
+
+  def self.down
+    remove_column :articles, :state
+    remove_column :articles, :key_timestamp
+
+    remove_index :articles, :name => :index_articles_on_state rescue ActiveRecord::StatementInvalid
+  end
+end
