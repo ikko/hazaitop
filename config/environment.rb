d6d740269dfd517ed3c51ab83dd8238bb94d9e07
Nandor Komzak <nandor.komzak@gmail.com>
2011-10-06 16:57:56 +0200

    bundlesítés, design kezdése, header megvan

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'hobo'
-  config.gem 'domainatrix'
+  #config.gem 'hobo'
+  #config.gem 'domainatrix'
 
   # Settings in config/environments/* take precedence over those specified here.
   # mainatrix'
