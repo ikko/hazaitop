4d617902998ce59eb0af35709df7b6cfcb5d1942
Nandor Komzak <nandor.komzak@gmail.com>
2011-10-13 15:46:40 +0200

    migrálás is

---
+++ db/migrate/20111013134628_hobo_migration_51.rb
@@ -0,0 +1,11 @@
+class HoboMigration51 < ActiveRecord::Migration
+  def self.up
+    change_column :people, :interpersonal_relations_count, :integer, :limit => 4, :default => 0
+    change_column :people, :person_to_org_relations_count, :integer, :limit => 4, :default => 0
+  end
+
+  def self.down
+    change_column :people, :interpersonal_relations_count, :integer
+    change_column :people, :person_to_org_relations_count, :integer
+  end
+end
