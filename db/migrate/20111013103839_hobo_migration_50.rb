421c34ebb341230df5ebe4cf139784a8bc0a3435
Nandor Komzak <nandor.komzak@gmail.com>
2011-10-13 15:45:49 +0200

    person indexen ráncfelvarrás, javítás

---
+++ db/migrate/20111013103839_hobo_migration_50.rb
@@ -0,0 +1,11 @@
+class HoboMigration50 < ActiveRecord::Migration
+  def self.up
+    Person.all.each do |person|
+      Person.update_counters person.id, :person_to_org_relations_count => 0 unless person.person_to_org_relations_count
+      Person.update_counters person.id, :interpersonal_relations_count => 0 unless person.interpersonal_relations_count
+    end
+  end
+
+  def self.down
+  end
+end
