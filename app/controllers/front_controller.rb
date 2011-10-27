cf75b7253d341a8218c0e66a6a9a49c822f9a996
Nandor Komzak <nandor.komzak@gmail.com>
2011-10-27 15:09:06 +0200

    főoldalon lapozhatóság

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -8,7 +8,20 @@ class FrontController < ApplicationController
 
   def index
     @people        = Person.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
-    @organizations = Organization.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source))
+    @organizations = Organization.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
+    @contracts     = Contract.apply_scopes(:order_by => parse_sort_param(:name)).paginate(:per_page=>10, :page=>params[:page])
+  end
+
+  index_action :person_pagination do
+    @people = Person.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
+  end
+
+  index_action :org_pagination do
+    @organizations = Organization.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
+  end
+
+  index_action :contract_pagination do
+    @contracts = Contract.apply_scopes(:order_by => parse_sort_param(:name)).paginate(:per_page=>10, :page=>params[:page])
   end
 
   def impressum; end
