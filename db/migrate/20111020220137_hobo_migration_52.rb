b918609216a90805544e412b8c35cb7eb34ec621
Nandor Komzak <nandor.komzak@gmail.com>
2011-10-29 13:51:42 +0200

    javítások

---
+++ db/migrate/20111020220137_hobo_migration_52.rb
@@ -0,0 +1,13 @@
+class HoboMigration52 < ActiveRecord::Migration
+  def self.up
+    add_column :organizations, :merge_from_id, :integer
+
+    add_index :organizations, [:merge_from_id]
+  end
+
+  def self.down
+    remove_column :organizations, :merge_from_id
+
+    remove_index :organizations, :name => :index_organizations_on_merge_from_id rescue ActiveRecord::StatementInvalid
+  end
+end
