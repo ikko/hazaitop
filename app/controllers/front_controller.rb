3de5f1412fd9ea74a51811221447db4124412e84
Nandor Komzak <nandor.komzak@gmail.com>
2011-11-02 14:00:22 +0100

    nincs fieldek szerinti sorrendezés lapozásoknál főoldalon

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -13,17 +13,15 @@ class FrontController < ApplicationController
   end
 
   index_action :person_pagination do
-    @people = Person.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
+    @people = Person.listed.paginate(:per_page=>10, :page=>params[:page])
   end
 
   index_action :org_pagination do
-    @organizations = Organization.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source)).paginate(:per_page=>10, :page=>params[:page])
+    @organizations = Organization.listed.paginate(:per_page=>10, :page=>params[:page])
   end
 
   index_action :contract_pagination do
-    @contracts = Contract.apply_scopes(:order_by => parse_sort_param(:name)).paginate(:per_page=>10, :page=>params[:page])
-    @people        = Person.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source))
-    @organizations = Organization.list.apply_scopes(:order_by => parse_sort_param(:name, :information_source))
+    @contracts = Contract.paginate(:per_page=>10, :page=>params[:page])
   end
 
   def impressum; end
