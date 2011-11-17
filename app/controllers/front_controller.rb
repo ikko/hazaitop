a4e848c673de75cd57a994ea1946d85966bcf890
Nandor Komzak <nandor.komzak@gmail.com>
2011-11-17 13:29:48 +0100

    ajax-paginate

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -20,6 +20,10 @@ class FrontController < ApplicationController
     @organizations = Organization.apply_scopes(:order_by => parse_sort_param(:person_to_org_relations_count, :interorg_relations_count, :updated_at)).paginate(:per_page=>10, :page=>params[:page])
   end
 
+  index_action :trans_pagination do
+  #  @transactions =
+  end
+
   index_action :contract_pagination do
     @contracts = Contract.apply_scopes(:order_by => parse_sort_param(:contracted_value, :no_of_proposals)).paginate(:per_page=>10, :page=>params[:page])
   end
@@ -37,12 +41,35 @@ class FrontController < ApplicationController
     # főoldalről érkező ajaxos keresés
     if request.xhr?
       query = "" unless query.present? 
-      @organizations = Organization.search(query, :name).paginate(:per_page=>10, :page=>1)
-      @people        = Person.search(query, :last_name, :first_name).paginate(:per_page=>10, :page=>1)
-      @litigations   = Litigation.search(query, :name).paginate(:per_page=>10, :page=>1)
-      @articles      = Article.search(query, :title).paginate(:per_page=>10, :page=>1)
+      @organizations = Organization.search(query, :name).paginate(:per_page=>10, :page=>params[:page])
+      @people        = Person.search(query, :last_name, :first_name).paginate(:per_page=>10, :page=>params[:page])
+      @litigations   = Litigation.search(query, :name).paginate(:per_page=>10, :page=>params[:page])
+      @articles      = Article.search(query, :title).paginate(:per_page=>10, :page=>params[:page])
       render_tags(@organizations+@people+@litigations+@articles, :search_card, :for_type => true) 
-    # részletes keresés oldalról érkező html keresés
+    end
+  end
+
+  # részletes keresés oldalról érkező html keresés
+  def detailed_search
+    query = params[:query]
+    # ha ajaxos lapozás van
+    if params[:block]
+      if params[:block]=='transaction'
+        @transactions = get_transactions
+        render "paginate_transaction" and return
+      elsif params[:block]=='person'
+        @people = get_people
+        render "paginate_person" and return
+      elsif params[:block]=='org'
+        @organizations = get_organizations
+        render "paginate_organization" and return
+      elsif params[:block]=='article'
+        @articles = get_articles
+        render "paginate_article" and return
+      elsif params[:block]=='litigation'
+        @litigations = get_litigations
+        render "paginate_litigation" and return
+      end
     else
       # ha nincs params-ba semmi akkor pl a főoldalról jöhetett, ilyenkor az alapbeállításokkal dolgozunk
       if !params[:transaction] && !params[:article] && !params[:person] && !params[:organization] && !params[:litigation]
@@ -54,98 +81,131 @@ class FrontController < ApplicationController
 
       # ha tranzakció be van jelölve akkor csak ott keresünk
       if params[:transaction]
-        transaction_conditions = []
-        transaction_conditions << ["interorg_relations.value >= ?", params[:amount_from]] if params[:amount_from]
-        transaction_conditions << ["interorg_relations.value <= ?", params[:amount_to]] if params[:amount_to]
-        transaction_conditions << ["interorg_relations.happened_at >= ?", params[:date_from]] if params[:date_from]
-        transaction_conditions << ["interorg_relations.happened_at <= ?", params[:date_to]] if params[:date_to]
-        cond = ""
-        par = []
-        transaction_conditions.each_with_index do |e, i|
-          if i.even?
-            cond << (i>0 ? " and #{e}" : e)
-          else
-            par << e
-          end
-        end
-        builded_transaction_conditions = [cond] + par
-        @transactions = Organization.search(query, :name).
-                                     paginate(:joins=>:interorg_relations, 
-                                              :conditions=>builded_transaction_conditions, 
-                                              :per_page=>10, 
-                                              :page=>1)
+        @transactions = get_transactions
       # nem kerestek dátumra
       elsif !params[:date_from].present? && !params[:date_to].present?
-        @organizations = Organization.search(query, :name).paginate(:per_page=>10, :page=>1) if params[:organization]
-        @people        = Person.search(query, :last_name, :first_name).paginate(:per_page=>10, :page=>1) if params[:person]
-        @litigations   = Litigation.search(query, :name).paginate(:per_page=>10, :page=>1) if params[:litigation]
-        @articles      = Article.search(query, :title).paginate(:per_page=>10, :page=>1) if params[:article]
+        @organizations = params[:organization] ? Organization.search(query, :name).paginate(:per_page=>10, :page=>params[:page]) : Organization.limit(0)
+        @people        = params[:person] ? Person.search(query, :last_name, :first_name).paginate(:per_page=>10, :page=>params[:page]) : Person.limit(0)
+        @litigations   = params[:litigation] ? Litigation.search(query, :name).paginate(:per_page=>10, :page=>params[:page]) : Litigation.limit(0)
+        @articles      = params[:article] ? Article.search(query, :title).paginate(:per_page=>10, :page=>params[:page]) : Article.limit(0)
       # valamire rákerestek
       else
-        person_conditions = []
-        organization_conditions = []
-        litigation_conditions = []
-        person_pars = []
-        org_pars = []
-        lit_pars = []
-
-        if params[:date_from].present?
-          person_conditions << "(person_to_org_relations.start_time >= ? or interpersonal_relations.start_time >= ?)"
-          person_pars << params[:date_from]
-          person_pars << params[:date_from]
-
-          organization_conditions << "person_to_org_relations.start_time >= ?"
-          org_pars << params[:date_from]
-
-          litigation_conditions << "start_time >= ?"
-          lit_pars << params[:date_from]
-        end
-        if params[:date_to].present?
-          person_conditions << "(person_to_org_relations.end_time <= ? or interpersonal_relations.end_time <= ?)"
-          person_pars << params[:date_to]
-          person_pars << params[:date_to]
-          organization_conditions << "person_to_org_relations.end_time <= ?"
-          org_pars << params[:date_to]
-          litigation_conditions << "end_time <= ?"
-          lit_pars << params[:date_to]
-        end
-
-        cond = ""
-        person_conditions.flatten.each_with_index do |e, i|
-          cond << (i>0 ? " and #{e}" : e)
-        end
-        builded_person_conditions = [cond] + person_pars
-
-        cond = ""
-        organization_conditions.flatten.each_with_index do |e, i|
-          cond << (i>0 ? " and #{e}" : e)
-        end
-        builded_organization_conditions = [cond] + org_pars
-
-        cond = ""
-        litigation_conditions.flatten.each_with_index do |e, i|
-          cond << (i>0 ? " and #{e}" : e)
-        end
-        builded_litigation_conditions = [cond] + org_pars
-
-        @people = params[:person] ? Person.search(query, :last_name, :first_name).
-                                           paginate(:joins=>"left outer join person_to_org_relations on person_to_org_relations.person_id = people.id 
-                                                             left outer join interpersonal_relations on interpersonal_relations.person_id = people.id", 
-                                                    :conditions=>builded_person_conditions,
-                                                    :per_page=>10, 
-                                                    :page=>1) : Person.limit(0)
-        @organizations = params[:organization] ? Organization.search(query, :name).paginate(:joins=>"left outer join person_to_org_relations on person_to_org_relations.organization_id = organizations.id",
-                                                                    :conditions=>builded_organization_conditions,
-                                                                    :per_page=>10,
-                                                                    :page=>1) : Organization.limit(0)
-        @litigations = params[:litigation] ? Litigation.search(query, :name).
-                                    paginate(:conditions=>builded_litigation_conditions, 
-                                             :per_page=>10, 
-                                             :page=>1) : Litigation.limit(0)
-
+        @people = params[:person] ? get_people : Person.limit(0)
+        @organizations = params[:organization] ? get_organizations : Organization.limit(0)
+        @litigations = params[:litigation] ? get_litigations : Litigation.limit(0)
         # article esetén nem figyeljük a dátum keresést
-        @articles = params[:article] ? Article.search(query, :title).paginate(:per_page=>10, :page=>1) : Article.limit(0)
+        @articles = params[:article] ? get_articles : Article.limit(0)
       end
     end
+  end
+
+private
+  def get_transactions
+    transaction_conditions = []
+    transaction_conditions << ["interorg_relations.value >= ?", params[:amount_from]] if params[:amount_from].present?
+    transaction_conditions << ["interorg_relations.value <= ?", params[:amount_to]] if params[:amount_to].present?
+    transaction_conditions << ["interorg_relations.happened_at >= ?", params[:date_from]] if params[:date_from].present?
+    transaction_conditions << ["interorg_relations.happened_at <= ?", params[:date_to]] if params[:date_to].present?
+    cond = ""
+    par = []
+    transaction_conditions.flatten.each_with_index do |e, i|
+      if i.even?
+        puts e.inspect
+        cond << (i>0 ? " and #{e}" : e)
+      else
+        par << e
+      end
+    end
+    builded_transaction_conditions = [cond] + par
+    Organization.search(params[:query], :name).
+                 paginate(:joins=>:interorg_relations, 
+                          :conditions=>builded_transaction_conditions, 
+                          :per_page=>10, 
+                          :page=>params[:page])
+  end
+
+  def get_people
+      person_conditions = []
+      person_pars = []
+      
+      if params[:date_from].present?
+        person_conditions << "(person_to_org_relations.start_time >= ? or interpersonal_relations.start_time >= ?)"
+        person_pars << params[:date_from]
+        person_pars << params[:date_from]
+
+      end
+      if params[:date_to].present?
+        person_conditions << "(person_to_org_relations.end_time <= ? or interpersonal_relations.end_time <= ?)"
+        person_pars << params[:date_to]
+        person_pars << params[:date_to]
+      end
+
+      cond = ""
+      person_conditions.flatten.each_with_index do |e, i|
+        cond << (i>0 ? " and #{e}" : e)
+      end
+      builded_person_conditions = [cond] + person_pars
+      Person.search(params[:query], :last_name, :first_name).
+             paginate(:joins=>"left outer join person_to_org_relations on person_to_org_relations.person_id = people.id 
+                               left outer join interpersonal_relations on interpersonal_relations.person_id = people.id", 
+                      :conditions=>builded_person_conditions,
+                      :per_page=>10,
+                      :page=>params[:page])
+  end
+
+  def get_organizations
+      organization_conditions = []
+      org_pars = []
+
+      if params[:date_from].present?
+        organization_conditions << "person_to_org_relations.start_time >= ? and interorg_relations.happened_at >=?"
+        org_pars << params[:date_from]
+        org_pars << params[:date_from]
+      end
+      if params[:date_to].present?
+        organization_conditions << "person_to_org_relations.end_time <= ? and interorg_relations.happened_at <=?"
+        org_pars << params[:date_to]
+        org_pars << params[:date_to]
+      end
+
+      cond = ""
+      organization_conditions.flatten.each_with_index do |e, i|
+        cond << (i>0 ? " and #{e}" : e)
+      end
+      builded_organization_conditions = [cond] + org_pars
+      Organization.search(params[:query], :name).
+                   paginate(:joins=>"left outer join person_to_org_relations on person_to_org_relations.organization_id = organizations.id
+                                     left outer join interorg_relations on interorg_relations.organization_id = organizations.id",
+                            :conditions=>builded_organization_conditions,
+                            :per_page=>10,
+                            :page=>params[:page])
+  end
+
+  def get_litigations
+      litigation_conditions = []
+      lit_pars = []
+
+      if params[:date_from].present?
+        litigation_conditions << "start_time >= ?"
+        lit_pars << params[:date_from]
+      end
+      if params[:date_to].present?
+        litigation_conditions << "end_time <= ?"
+        lit_pars << params[:date_to]
+      end
+
+      cond = ""
+      litigation_conditions.flatten.each_with_index do |e, i|
+        cond << (i>0 ? " and #{e}" : e)
+      end
+      builded_litigation_conditions = [cond] + org_pars
+      Litigation.search(params[:query], :name).
+                 paginate(:conditions=>builded_litigation_conditions, 
+                          :per_page=>10, 
+                          :page=>params[:page])
+  end
+
+  def get_articles
+    Article.search(params[:query], :title).paginate(:per_page=>10, :page=>params[:page])
   end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,7 +23,7 @@ class OrganizationsController < ApplicationController
   index_action :person_to_org_pagination do
     @this = find_instance
     return unless @this
-    @person_to_orgs = @this.interorg_relations.paginate(:per_page=>10, :page=>params[:page])
+    @person_to_orgs = @this.person_to_org_relations.paginate(:per_page=>10, :page=>params[:page])
   end
 
   index_action :interorg_pagination do
@@ -49,7 +49,7 @@ class OrganizationsController < ApplicationController
 
   def show
     @this               = find_instance
-    @person_to_org_relations = @this.interorg_relations.paginate(:per_page=>10, :page=>params[:page])
+    @person_to_org_relations = @this.person_to_org_relations.paginate(:per_page=>10, :page=>params[:page])
     @interorg_relations = @this.interorg_relations.paginate(:per_page=>10, :page=>params[:page])
     @financials = @this.org_histories.paginate(:per_page=>10, :page=>params[:page])
     respond_to do |format| 
