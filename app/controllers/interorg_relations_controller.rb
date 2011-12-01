06dbaad578ffa16a6316aa08fb134b9d44180061
Nandor Komzak <nandor.komzak@gmail.com>
2011-12-01 14:21:30 +0100

    tranzakciók mutatása contract helyett index oldalon

--- app/controllers/interorg_relations_controller.rb
+++ app/controllers/interorg_relations_controller.rb
@@ -13,5 +13,13 @@ class InterorgRelationsController < ApplicationController
     end
   end
 
+  def index
+    @this = InterorgRelation.value_is_not('').order_by(:value, 'desc').paginate(:per_page=>10, :page=>1)
+  end
+
+  index_action :list do
+    hobo_index InterorgRelation.value_is_not('').order_by(params['sort'].to_sym)
+    render :index
+  end
 
 end
