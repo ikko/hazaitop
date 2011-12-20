75c945cd7e8771c560f20507a289751daef85226
Béky Miklós <miklos.beky@gmail.com>
2011-12-20 17:26:27 +0100

    migrations

---
+++ db/migrate/20111220155014_hobo_migration_69.rb
@@ -0,0 +1,13 @@
+class HoboMigration69 < ActiveRecord::Migration
+  def self.up
+    add_column :interorg_relations, :parsed, :boolean, :default => false
+
+    add_column :person_to_org_relations, :parsed, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :interorg_relations, :parsed
+
+    remove_column :person_to_org_relations, :parsed
+  end
+end
