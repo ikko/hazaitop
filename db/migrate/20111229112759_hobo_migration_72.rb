d9c518c0a51e875bb580a5f94dd8bad6b3c6bb17
Béky Miklós <miklos.beky@gmail.com>
2011-12-29 12:41:06 +0100

    migrate

---
+++ db/migrate/20111229112759_hobo_migration_72.rb
@@ -0,0 +1,13 @@
+class HoboMigration72 < ActiveRecord::Migration
+  def self.up
+    add_column :o2p_relation_types, :mirror_of_id, :integer
+
+    add_index :o2p_relation_types, [:mirror_of_id]
+  end
+
+  def self.down
+    remove_column :o2p_relation_types, :mirror_of_id
+
+    remove_index :o2p_relation_types, :name => :index_o2p_relation_types_on_mirror_of_id rescue ActiveRecord::StatementInvalid
+  end
+end
